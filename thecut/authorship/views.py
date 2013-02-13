565df940e7b16d972d98c017598dd20c7ddcfeab
Matt Austin <matt.austin@thecut.net.au>
2013-02-13 08:58:43 +0800

    Updated for docstrings on ``AuthorshipFormMixin``.

--- thecut/authorship/views.py
+++ thecut/authorship/views.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 
 
 class AuthorshipViewMixin(object):
-    """Add the request's ``User`` instance to the form kwargs."""
+    """Adds the request's ``User`` instance to the form kwargs."""
 
     def get_form_kwargs(self, *args, **kwargs):
         form_kwargs = super(AuthorshipViewMixin, self).get_form_kwargs(
