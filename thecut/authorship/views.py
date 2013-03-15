2d4a8eda68a5823950c36199441497e0e271c0fe
Matt Austin <matt.austin@thecut.net.au>
2013-03-15 08:03:56 +0800

    Renamed model/view mixin/form mixin for consistency with django and other apps.

--- thecut/authorship/views.py
+++ thecut/authorship/views.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+import warnings
 
 
-class AuthorshipViewMixin(object):
+class AuthorshipMixin(object):
     """Adds the request's ``User`` instance to the form kwargs."""
 
     def get_form_kwargs(self, *args, **kwargs):
-        form_kwargs = super(AuthorshipViewMixin, self).get_form_kwargs(
-            *args, **kwargs)
+        form_kwargs = super(AuthorshipMixin, self).get_form_kwargs(*args,
+                                                                   **kwargs)
         form_kwargs.update({'user': self.request.user})
         return form_kwargs
+
+
+class AuthorshipViewMixin(AuthorshipMixin):
+    """AuthorshipViewMixin is deprecated. Use AuthorshipMixin instead."""
+    # Renamed for consistency with other mixins.
+
+    def __init__(self, *args, **kwargs):
+        warnings.warn('AuthorshipViewMixin is deprecated - use '
+                      'AuthorshipMixin instead.', DeprecationWarning,
+                      stacklevel=2)
+        return super(AuthorshipViewMixin, self).__init__(*args, **kwargs)
