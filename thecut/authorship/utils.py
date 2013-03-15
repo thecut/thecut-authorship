ef9b2e977443845dc6120c30976712cc8c3ac41a
Matt Austin <matt.austin@thecut.net.au>
2013-03-15 08:39:03 +0800

    Added ``get_website_user`` util function (moved and updated from ``thecut.core``).

---
+++ thecut/authorship/utils.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from django.contrib.auth import get_user_model
+from thecut.authorship import settings
+
+
+def get_website_user():
+    """Get a generic 'website' user.
+
+    Can be used to specify the required user when there is no direct link to
+    a real user.
+
+    """
+
+    UserModel = get_user_model()
+    user, created = UserModel.objects.get_or_create(**settings.WEBSITE_USER)
+
+    if created:
+        user.set_unusable_password()
+        user.is_active = False
+        user.save()
+
+    return user
