7a9dc2e1820a2440f6e04f64f260e20333e6704a
Josh Crompton <josh.crompton@thecut.net.au>
2014-07-08 14:36:03 +0800

    Use factories provided by the authorship app where possible.
    Instead of (re)definining factories specifically for the tests,
    use the ones provided by the app itself.

--- test_app/factories.py
+++ test_app/factories.py
@@ -1,22 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from .models import AuthorshipModel
-import factory
+from thecut.authorship.factories import AuthorshipFactory
 
 
-class AuthorshipModelFactory(factory.DjangoModelFactory):
-
-    created_by = factory.SubFactory('test_app.factories.UserFactory')
-    updated_by = factory.LazyAttribute(lambda o: o.created_by)
+class AuthorshipModelFactory(AuthorshipFactory):
 
     class Meta(object):
         model = AuthorshipModel
-
-
-class UserFactory(factory.DjangoModelFactory):
-
-    username = factory.Sequence(lambda n: 'User {0}'.format(n))
-
-    class Meta(object):
-        model = 'auth.User'
-        django_get_or_create = ('username',)
--- thecut/authorship/tests/test_models.py
+++ thecut/authorship/tests/test_models.py
@@ -1,30 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from django.test import TestCase
-from test_app.factories import AuthorshipModelFactory, UserFactory
+from test_app.factories import AuthorshipModelFactory as AuthorshipFactory
+from thecut.authorship.factories import UserFakerFactory
 
 
 class TestAuthorshipModel(TestCase):
 
     def test_sets_created_by_when_model_instance_is_first_saved(self):
-        authored = AuthorshipModelFactory.build()
-        user = UserFactory()
+        authored = AuthorshipFactory.build()
+        user = UserFakerFactory()
 
         authored.save(user=user)
 
         self.assertEqual(user, authored.created_by)
 
     def test_sets_updated_by_when_model_instance_is_saved(self):
-        authored = AuthorshipModelFactory()
-        update_user = UserFactory(username='update user')
+        authored = AuthorshipFactory()
+        update_user = UserFakerFactory(username='update user')
 
         authored.save(user=update_user)
 
         self.assertEqual(update_user, authored.updated_by)
 
     def test_does_not_change_created_by_when_model_instance_is_saved(self):
-        authored = AuthorshipModelFactory()
-        update_user = UserFactory(username='update user')
+        authored = AuthorshipFactory()
+        update_user = UserFakerFactory(username='update user')
 
         authored.save(user=update_user)
 
