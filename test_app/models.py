7295a1f84ccfd061f99cea8e32d063ce3764a005
Josh Crompton <josh.crompton@thecut.net.au>
2014-07-08 14:28:24 +0800

    Add tests for Authorship model.

---
+++ test_app/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from thecut.authorship.models import Authorship
+
+
+class AuthorshipModel(Authorship):
+
+    pass
