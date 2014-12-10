862a4aa29c5ce8d314662a9f77e391f02c7785c9
Matt Austin <matt.austin@thecut.net.au>
2014-12-10 11:45:33 +0800

    AuthorshipMixin now inherits from AuthorshipInlineMixin.

--- thecut/authorship/admin.py
+++ thecut/authorship/admin.py
@@ -3,18 +3,6 @@ from __future__ import absolute_import, unicode_literals
 from .models import Authorship
 
 
-class AuthorshipMixin(object):
-    """Mixin for a model admin to set created/updated by on save."""
-
-    def save_model(self, request, obj, form, change, *args, **kwargs):
-        # Set created_by / updated_by fields using request.user
-        if not change:
-            obj.created_by = request.user
-        obj.updated_by = request.user
-        return super(AuthorshipMixin, self).save_model(
-            request, obj, form, change, *args, **kwargs)
-
-
 class AuthorshipInlineMixin(object):
     """Mixin for a model admin to set created/updated by on save for related
     inline models."""
@@ -25,6 +13,20 @@ class AuthorshipInlineMixin(object):
             for instance in instances:
                 instance.save(user=request.user)
             formset.save_m2m()
+            for obj in formset.deleted_objects:
+                obj.delete()
         else:
             return super(AuthorshipInlineMixin, self).save_formset(
                 request, form, formset, *args, **kwargs)
+
+
+class AuthorshipMixin(AuthorshipInlineMixin):
+    """Mixin for a model admin to set created/updated by on save."""
+
+    def save_model(self, request, obj, form, change, *args, **kwargs):
+        # Set created_by / updated_by fields using request.user
+        if not change:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        return super(AuthorshipMixin, self).save_model(request, obj, form,
+                                                       change, *args, **kwargs)
