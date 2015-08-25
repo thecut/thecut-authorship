aacb702eaede9b82a8117c784397836ffb684ce0
Matt Austin <matt.austin@thecut.net.au>
2015-08-25 10:43:55 +0800

    Set ``on_delete`` to ``models.PROTECT`` for ``created_by`` and ``updated_by`` ForeignKey fields. ``on_delete`` will become a required parameter in Django 1.9. Fixes #5401.

--- thecut/authorship/models.py
+++ thecut/authorship/models.py
@@ -13,14 +13,14 @@ class Authorship(models.Model):
     """:py:class:`datetime` for when this object was first created."""
 
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, editable=False,
-                                   related_name='+')
+                                   related_name='+', on_delete=models.PROTECT)
     """User who first created this object (required)."""
 
     updated_at = models.DateTimeField(auto_now=True, editable=False)
     """:py:class:`datetime` for when this object was last saved."""
 
     updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, editable=False,
-                                   related_name='+')
+                                   related_name='+', on_delete=models.PROTECT)
     """User who last saved this object (required)."""
 
     class Meta(object):
