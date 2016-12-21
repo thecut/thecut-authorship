463120d3570f264b540600a72fbdb98b5fda82b3
Mark Lockett <mark@deliveryengine.net>
2016-12-21 16:10:37 +0800

    Revert "Require Faker"
    This reverts commit b30d5ec11292c23817b4260214b42c1892bee48a.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     include_package_data=True,
 
     # Dependencies
-    install_requires=['Faker>=0.5.7'],
+    install_requires=[],
 
     # Author information
     author='The Cut Creative',
