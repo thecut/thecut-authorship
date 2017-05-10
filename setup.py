f0d08ed1af917d935ed6bb5613a3a2d0b98ee7ff
Matt Austin <matt.austin@thecut.net.au>
2017-05-10 13:18:37 +0800

    Add python version trove classifiers.

--- setup.py
+++ setup.py
@@ -48,6 +48,12 @@ setup(
     platforms='any',
     classifiers=[
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Development Status :: 5 - Production/Stable',
         'Natural Language :: English',
         'Environment :: Web Environment',
